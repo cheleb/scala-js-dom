56ce1a2407d9a923e07c066d7064947b68535c3c
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-02-23 15:25:55 +0100

    Update dependencies to Scala3 LTS

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
   object Ver {
     val scala211 = "2.11.12"
     val scala212 = "2.12.15"
-    val scala213 = "2.13.6"
-    val scala3   = "3.1.3"
+    val scala213 = "2.13.16"
+    val scala3   = "3.3.5"
   }
 
   object Dep {
